09646de3de3e37c58d8a6259bbeab469ae836878
AbinRagab <ahmderagabe@gmail.com>
2024-08-25 14:37:44 +0300

    Signin Ui design

---
+++ frontend/src/components/PasswordCheck.jsx
@@ -0,0 +1,91 @@
+import { Check, X } from 'lucide-react'
+
+const PasswordCriteria = ({password})=>{
+    
+    console.log(password);
+    
+    console.log(/[A-Z]/.test(password) );
+    console.log(/[a-z]/.test(password) );
+    console.log(/^[a-zA-Z0-9]/.test(password));
+    
+
+    const criteria = [
+        {label: "At least 6 chars", met: password?.length >= 6 },
+        {label: "contains LowerCase Letter", met: /[a-z]/.test(password) },
+        {label: "contains UpperCase Letter", met: /[A-Z]/.test(password) },
+        {label: "contains A number", met: /\d/.test(password) },
+        {label: "not contains Special characters", met: /^[a-zA-Z0-9]/.test(password) }
+    ]
+
+    return(
+        <div className='mt-2 space-y-1'>
+                {criteria.map((item)=>{
+                  return <div key={item.label} className='flex items-center text-xs'>
+                            {item.met?
+                                <Check className='text-green-500 mr-2' />
+                            :
+                                <X className='text-gray-400 mr-2' />
+                            }
+            
+                                <span className={item.met? "text-green-500" : "text-gray-400"}>{item.label}</span>
+                                                        
+                    </div>     
+                })}
+        </div>
+    )
+}
+
+const PasswordCheck = ({password}) => {
+    
+    const strengthPassword = ({password})=>{
+            let strength = 0
+            if(password?.length >= 6) strength++
+            if(/[a-z]/.test(password) &&/[A-Z]/.test(password)) strength++
+            if( /\d/.test(password)) strength++
+            if(/^[a-zA-Z0-9]/.test(password)) strength++
+
+            return strength
+    }
+
+    const strength = strengthPassword({password})
+
+    const getStrengethText = (strength)=>{
+        if(strength == 0) return "very weak"
+        if(strength == 1) return "weak"
+        if(strength == 2) return "fair"
+        if(strength == 3) return "good"
+        return "Strong"
+    }
+
+
+    const getColor = (strength)=>{
+        if(strength == 0) return "bg-red-400"
+        if(strength == 1) return "bg-red-500"
+        if(strength == 2) return "bg-yellow-400"
+        if(strength == 3) return "bg-yellow-500"
+        return "bg-green-500"
+    }
+
+    
+    
+  return (
+    <div>
+        <div className='flex justify-between text-xs'>
+            <span className='text-gray-400'>Password Strength: </span>
+            <span className='text-gray-400'>{getStrengethText(strength)}</span>
+        </div>
+        <div className='flex space-x-1 mt-3'>
+            {[...Array(4)].map((_,idx)=>{
+               return  <span className={`h-1 w-1/4 rounded-full transition-colors duration-300 
+                    ${idx < strength ? getColor(strength) : "bg-gray-600"}
+                    `}></span>
+            })}
+        </div>
+        <PasswordCriteria password={password} />
+    </div>
+  )
+}
+
+export default PasswordCheck
+
+
--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react'
 import { animate, motion } from 'framer-motion';
 import Input from './../components/Input';
 import { Lock, Mail, Scale, User } from 'lucide-react';
+import { Link } from 'react-router-dom';
+import PasswordCheck from './../components/PasswordCheck';
+
 
 
 
@@ -14,11 +17,14 @@ export default function Signin() {
     const handelSubmit = (e)=>{
         e.preventDefault();
     }
+
+    console.log('hi');
+    
     return <motion.div
         initial= {{opacity: 0, y: 20}}
         animate = {{opacity: 1 , y: 0}}
         transition={{duration: 0.5}}
-        className='max-w-md w-full bg-gray-800 bg-opacity-50 rounded-2xl backdrop-filter backdrop-blur-xl px-8 shadow-xl overflow-hidden'
+        className='max-w-md w-full bg-gray-800 relative bg-opacity-50 pb-14 rounded-2xl backdrop-filter backdrop-blur-xl px-8 shadow-xl overflow-hidden'
     >
         <div className='pt-8 w-full'>
                 <h2 className=' bg-gradient-to-r text-transparent bg-clip-text font-bold text-center text-2xl from-green-700 to-green-500'>Create Account</h2>
@@ -69,6 +75,16 @@ export default function Signin() {
                  >
                     Sign Up
                  </motion.button>
+
+                 
         </form>
+        
+         {/*  */}
+                 <PasswordCheck password={password} />
+        {/* Footer Of Div SignUp */}
+                 <div className='bg-gray-900 bg-opacity-50 py-3 text-sm absolute right-0 left-0 bottom-0 w-full flex justify-center text-gray-400 '>
+                    <p>Already have an account?</p>
+                    <Link className= 'ml-2 text-green-600 hover:underline ' to = {''}>Login</Link>
+                 </div>
     </motion.div>
 }
